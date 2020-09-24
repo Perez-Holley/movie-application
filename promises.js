fc5ba00c4d3c709d65008fccaeca338aafa2cc28
Unknown <blossomperez18@gmail.com>
2020-09-23 20:30:36 -0500

    Allow for additions to server

--- promises.js
+++ promises.js
@@ -3,13 +3,18 @@ const glitchUrl = 'https://feather-satisfying-sprite.glitch.me/movies'
 
 let i = ["tt0077651", "tt0080761"]
 
-const addMovieFromAPI = i => {
+const addMovieFromAPI = (i) => {
     return i.forEach(movie => {
-        fetch(`http://www.omdbapi.com/?apikey=${OMDb}&i=${i}`)
+        fetch(`http://www.omdbapi.com/?apikey=${OMDb}&i=${movie}`)
             .then(res => res.json())
             .then(console.log)
     })
 }
+
+
+fetch(glitchUrl)
+.then(res => res.json())
+.then(console.log)
 
 
 // // edit
@@ -28,33 +33,33 @@ const addMovieFromAPI = i => {
 // // editMovies()
 //
 //
-//     // delete
-//     const deleteMovie = id => fetch(`${glitchUrl}/${id}`, {
-//         method: 'DELETE',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//         .then(res => res.json())
-//         .then(() => {
-//             console.log(`Success: deleted movie with id of ${id}`);
-//         })
-//         .catch(console.error);
-//
-//         // deleteMovie()
+    // delete
+    const deleteMovie = id => fetch(`${glitchUrl}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(() => {
+            console.log(`Success: deleted movie with id of ${id}`);
+        })
+        .catch(console.error);
+
+        deleteMovie(37)
 //
 //     //create
 //
-//     const addMovie = (movie) => fetch(`${glitchUrl}`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(movie)
-//     })
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(`Success: created ${JSON.stringify(data)}`);
-//             return data.id; // to access the primary key of the newly created entity
-//         })
-//         .catch(console.error);
+    const addMovie = (movie) => fetch(`${glitchUrl}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movie)
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(`Success: created ${JSON.stringify(data)}`);
+            return data.id; // to access the primary key of the newly created entity
+        })
+        .catch(console.error);
